0be5a576a609942de7bf294975292a92ffc86d4f
Huy <phamanhhuy22@gmail.com>
2023-01-08 21:35:18 +0700

    Cấu hình CSS layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/components/Layout';
 
